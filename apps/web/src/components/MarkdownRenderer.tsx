0bfb54f51e597da88a5c7bcef8cc3420eb9b68d3
Junaid Shaikh <junaidshaikhh03@gmail.com>
2026-07-25 01:01:41 +0530

    feat: create base MarkdownRenderer component with markdown parsing

---
+++ apps/web/src/components/MarkdownRenderer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+interface MarkdownRendererProps {
+  content: string;
+}
+
+export const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      className="prose prose-sm max-w-none dark:prose-invert"
+    >
+      {content}
+    </ReactMarkdown>
+  );
+};
